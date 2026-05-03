41906a1854faadc33e176cb3f363790667cd147b
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-05-03 12:40:38 +0600

    running

--- react-frontend/src/pages/Product.jsx
+++ react-frontend/src/pages/Product.jsx
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { useContext } from 'react';
 import { ShopContext } from '../context/ShopContext'; 
 import { useParams } from 'react-router-dom'; 
+import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const {productId} = useParams();  
-  const {products} = useContext(ShopContext); 
+  const {products, currency} = useContext(ShopContext); 
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState(''); 
+  const [size, setSize] = useState('');
 
 
   const fetchProductData = async () => {
@@ -45,7 +48,67 @@ const Product = () => {
 
         </div>
 
+        <div className=' flex-1 '>
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+
+          <div className=' flex items-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className=' w-3 5'/>
+            <img src={assets.star_icon} alt="" className=' w-3 5'/>
+            <img src={assets.star_icon} alt="" className=' w-3 5'/>
+            <img src={assets.star_icon} alt="" className=' w-3 5'/>
+            <img src={assets.star_dull_icon} alt="" className=' w-3 5'/>
+
+            <p className='pl-2'>(122)</p>
+
+          </div>
+          <p className=' mt-5 text-3xl font-medium'>{currency} {productData.price}</p>
+
+          <p className=' mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+              {
+                productData.sizes.map((item, index) => (
+                  <button onClick={()=> setSize(item)} key={index} className={` border p-2 py-2 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`}>{item}</button>
+                ))
+              }
+
+            </div>
+
+          </div>
+
+          <button className=' bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+
+          <hr className=' mt-8 sm:w-4/5'/>
+          <div className=' text-sm text-gray-500 mt-8 flex flex-col gap-1'>
+            <p>100% Orginal product</p>
+            <p>Cash on delivery available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+
+          </div>
+
+        </div>
+
       </div>
+
+      <div className=' mt-20'>
+        <div className='flex'>
+          <b className='border border-gray-300 px-5 py-3 text-sm'>Description</b>
+          <p className=' border border-gray-300 px-5 py-3 text-sm'>Reviews (122)</p>
+
+        </div>
+
+        <div className='flex flex-col gap-4 border border-gray-300 px-6 py-6 text-sm text-gray-500'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia minima minus ipsa officiis assumenda molestias incidunt quisquam voluptatem earum quidem! Consequuntur quasi est cumque ab tempore alias. Vel, reiciendis. Veniam.</p>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe iusto vitae earum aliquam modi molestias accusamus sunt cupiditate impedit sit?</p>
+
+        </div>
+
+      </div>
+
+       <RelatedProducts category={productData.category} subCategory={productData.subCategory}></RelatedProducts>
       
     </div>
   ) : <div className=' opacity-0'></div>
